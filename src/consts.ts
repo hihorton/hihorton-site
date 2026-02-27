ff7881a5ce4ca4e72daff46dda0ba2ff5ad81f0c
Michael Horton <hihorton@proton.me>
2026-02-27 14:20:58 -0800

    fix: site descriptions and titles update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = 'hihorton';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
